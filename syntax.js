a696566596c021453805ccfe254cf77684064f12
asa-12 <160116670+asa-12@users.noreply.github.com>
2024-05-14 16:03:59 +0100

    Add files via upload

---
+++ syntax.js
@@ -0,0 +1,51 @@
+// 
+
+// const x = 5;
+// const y = 6;
+// const z = 4;
+// console.log(x < 10 && y < 1 && z >2);
+
+// const x = 5;
+// const y = 6;
+// const z = 4;
+// console.log(x > 10 || y < 1 || z > 2);
+
+// const a = true;
+// console.log(!a);
+// const b = false;
+// console.log(!b);
+
+// const a =false;
+// console.log(!!a);
+
+// let age = 18;
+
+// let vartable = (age <= 18) ? "too young" : "old enagh";
+
+// console.log(vartable);
+
+// let length = 16;
+// console.log(typeof length);
+// let weight = 7.5;
+
+
+
+// const string = "hello world!";
+
+// console.log(string.length);
+
+//  const a = " my name\t is \"Mishad\"";
+//  console.log(a);
+
+// const x = new String("mishad");
+// y = "mishad"
+// console.log(typeof x);
+// console.log(typeof y);
+// console.log(x===y);
+
+// const text = "mishad";
+// length = text.length;
+// console.log(length);
+const text = "hello world";
+const char = text.charAt(4)
+console.log(char);
